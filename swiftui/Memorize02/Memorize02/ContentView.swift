d288dba89a76de3a3d3a280862e5cd7460e41a11
tianfav <tianfav@163.com>
2024-12-07 13:02:36 +0800

    lecture02 完成 02 所有功能,详情看每个 view 的注释

--- swiftui/Memorize02/Memorize02/ContentView.swift
+++ swiftui/Memorize02/Memorize02/ContentView.swift
@@ -4,8 +4,10 @@
 //
 //  Created by shanquan on 2024/11/27.
 //
-// 基于 02 的 button，继续抽象 cardRemover 等几个计算属性，使得代码更加清晰易读
-// 可以跟 02 对比当前版本的改动
+// 将 hstask 调整为网格布局， 增加了视图滚动效果
+// 将 isFaceUp 的 if/else 逻辑使用 透明度实现，解决 LazyVGrid 在无图像时压缩网格单元的情况
+// 使用 aspectRatio 来控制网格单元的宽高比
+// 可以跟 03 对比当前版本的改动
 
 import SwiftUI
 
@@ -18,42 +20,45 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            cards
+            ScrollView{
+                cards
+            }
+            Spacer()
             cardAdjuster
         }
         .padding()
     }
-    var cardRemover: some View {
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
         Button(
             action: {
-                if cardCount > 1 {
-                    cardCount -= 1
-                }
+                cardCount += offset
             },
             label: {
-                Image(systemName: "rectangle.stack.badge.minus.fill")
-            })
+                Image(systemName: symbol)
+            }
+        )
         .font(.largeTitle)
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    var cardRemover: some View {
+        return cardCountAdjuster(
+            by: -1, symbol: "rectangle.stack.badge.minus.fill")
     }
     var cardAdder: some View {
-        Button(
-            action: {
-                if cardCount < emojis.count {
-                    cardCount += 1
-                }
-            },
-            label: {
-                Image(systemName: "rectangle.stack.badge.plus.fill")
-            })
-        .font(.largeTitle)
+        return cardCountAdjuster(
+            by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 
-    //隐式返回,下面的 return 可以省略
-    // 如果你有一个普通的函数并且它只有一行代码，那么你可以省略 return
-    var cards: some View{
-        return HStack {
+    var cards: some View {
+        //LazyVGrid 会使网格占用尽可能少的空间，如果没有表情时，其空间会被压缩，会展示 2 个黄条
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                //在 SwiftUI 中，aspectRatio 是一个用于控制视图（尤其是图像）纵横比的修饰符。它确保视图在调整大小时保持特定的宽高比，从而避免图像被拉伸或压缩
+                //aspectRatio：一个 CGFloat，表示宽度与高度的比例。
+                //contentMode：指定如何调整图像以适应给定的空间，常用的选项有 .fit 和 .fill。
+                    .aspectRatio(2/3,contentMode: .fit)
+
             }
         }
         .foregroundColor(.orange)
@@ -65,7 +70,7 @@ struct ContentView: View {
             Spacer()
             cardAdder
         }
-        
+
     }
 }
 
@@ -77,13 +82,13 @@ struct CardView: View {
         let base = RoundedRectangle(cornerRadius: 12)
 
         ZStack {
-            if isFaceUp {
+            Group {
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.fill()
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture(count: 1) {
             debugPrint("tapped")

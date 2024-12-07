7d66bcb6263e1623d06648d8f707e5ae501bedad
tianfav <tianfav@163.com>
2024-12-07 13:06:19 +0800

    lecture03 init

--- swiftui/Memorize03/Memorize03/ContentView.swift
+++ swiftui/Memorize03/Memorize03/ContentView.swift
@@ -46,13 +46,9 @@ struct ContentView: View {
     }
 
     var cards: some View {
-        //LazyVGrid 会使网格占用尽可能少的空间，如果没有表情时，其空间会被压缩，会展示 2 个黄条
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
-                //在 SwiftUI 中，aspectRatio 是一个用于控制视图（尤其是图像）纵横比的修饰符。它确保视图在调整大小时保持特定的宽高比，从而避免图像被拉伸或压缩
-                //aspectRatio：一个 CGFloat，表示宽度与高度的比例。
-                //contentMode：指定如何调整图像以适应给定的空间，常用的选项有 .fit 和 .fill。
                     .aspectRatio(2/3,contentMode: .fit)
 
             }

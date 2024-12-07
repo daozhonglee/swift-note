960078c6f88b9d29ed68eeae5f27454c90539720
tianfav <tianfav@163.com>
2024-12-07 12:05:33 +0800

    lecture02 增加 cardRemover 计算属性用于 card 增减

--- swiftui/Memorize02/Memorize02/ContentView.swift
+++ swiftui/Memorize02/Memorize02/ContentView.swift
@@ -4,38 +4,83 @@
 //
 //  Created by shanquan on 2024/11/27.
 //
+// 基于 02 的 button，继续抽象 cardRemover 计算属性，用于 card 的增减
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        HStack{
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
-        }
+    let emojis = [
+        "👻", "🧅", "👿", "🍊", "🍎", "🍍", "🍐", "🍓", "🍇", "🍉", "🍌",
+    ]
 
-        .foregroundColor(.orange)
-        .padding()
+    @State var cardCount = 4
+
+    var body: some View {
+        VStack {
+            HStack {
+                // 在 SwiftUI 中，ForEach 是一个非常有用的结构，用于动态生成视图
+                // 它允许开发者通过循环创建多个视图，而无需手动为每个元素编写代码。
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(content: emojis[index])
+                }
+            }
+            .foregroundColor(.orange)
+            .padding()
+            HStack {
+                cardRemover
+                Spacer()
+                cardAdder
+
+            }
+            .imageScale(.large)
+            .font(.largeTitle)
+        }
+    }
+    var cardRemover: some View {
+        Button(
+            action: {
+                if cardCount > 1 {
+                    cardCount -= 1
+                }
+            },
+            label: {
+                Image(systemName: "rectangle.stack.badge.minus.fill")
+            })
+    }
+    var cardAdder: some View {
+        Button(
+            action: {
+                if cardCount < emojis.count {
+                    cardCount += 1
+                }
+            },
+            label: {
+                Image(systemName: "rectangle.stack.badge.plus.fill")
+            })
     }
 }
+
 struct CardView: View {
-    var isFaceUp: Bool = false
+    let content: String
+    @State var isFaceUp: Bool = true
 
     var body: some View {
-        ZStack(content: {
-            if isFaceUp{
-            RoundedRectangle(cornerRadius: 12)
-                .foregroundColor(.white)
-            RoundedRectangle(cornerRadius: 12)
-                .strokeBorder(lineWidth: 2)
-            Text("👿").font(.largeTitle)
-            }else{
-                //当我没有指定 CardView 上的颜色时，上面的橙色实际上会一直渗透到 CardView 中，这是因为 SwiftUI 会将视图的前景色传递给它的子视图
-                RoundedRectangle(cornerRadius: 12)
+        let base = RoundedRectangle(cornerRadius: 12)
+
+        ZStack {
+            if isFaceUp {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
             }
-        })
+        }
+        .onTapGesture(count: 1) {
+            debugPrint("tapped")
+            isFaceUp.toggle()
+        }
+
     }
 }
 

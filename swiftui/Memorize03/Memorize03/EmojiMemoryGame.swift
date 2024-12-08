c3d27765d4aefcf0409308cabba093335d5eca05
tianfav <tianfav@163.com>
2024-12-08 09:55:45 +0800

    lecture03 分离Model 和 View,实现第一版MVVM

--- swiftui/Memorize03/Memorize03/EmojiMemoryGame.swift
+++ swiftui/Memorize03/Memorize03/EmojiMemoryGame.swift
@@ -4,17 +4,42 @@
 //
 //  Created by shanquan on 2024/12/7.
 //
+// 在 VM 中增加 Model 的初始化方法，
+// 使用函数式编程的方式，初始化 Model，并用尾随闭包的形式提供了初始化 model 的方法
+//
 
 import SwiftUI
 
-class EmojiMemoryGame{
+func createCardContent(index: Int) -> String {
+    return [
+        "👻", "🧅", "👿", "🍊", "🍎", "🍍", "🍐", "🍓", "🍇", "🍉", "🍌",
+    ][index]
+}
+
+class EmojiMemoryGame {
     //如果不加 private，那么 view 可以直接控制和修改 model
     //加 private 将 model 变成只有 VM 可以控制
     // 加了 private，那么 Model 和 View 就完全分离了，对应与在Model 使用 @State 的情况下，Model 和 View 是耦合的
-    private var model: MemoryGame<String>
-    
-    //class 默认会有一个无参的构造方法，但该构造方法只有在所有变量都有默认值的情况下才起作用，所以如果存在有没有默认值的属性，必须声明一个构造方法
+    // 第1种写法 非闭包写法
+    // private var model = MemoryGame(numberofPairsOfCards: 4, cardContentFactory: createCardContent)
+    // 第2种写法 闭包写法
+    //    private var model = MemoryGame(
+    //        numberofPairsOfCards: 4,
+    //        cardContentFactory: { (index) in
+    //            [
+    //                "👻", "🧅", "👿", "🍊", "🍎", "🍍", "🍐", "🍓", "🍇", "🍉", "🍌",
+    //            ][index]
+    //        })
+    // 第3种写法 尾随闭包写法：当最后一个参数是函数时，可以以闭包的形式将它放在外部
+    private var model = MemoryGame(
+        numberofPairsOfCards: 4
+    ) { (index) in
+        [
+            "👻", "🧅", "👿", "🍊", "🍎", "🍍", "🍐", "🍓", "🍇", "🍉", "🍌",
+        ][index]
+    }
 
+    //class 默认会有一个无参的构造方法，但该构造方法只有在所有变量都有默认值的情况下才起作用，所以如果存在有没有默认值的属性，必须声明一个构造方法
 
     //因为 model是 private 的，提供另一个计算属性用于获取 model 的 cards
     var cards: [MemoryGame<String>.Card] {
@@ -23,7 +48,7 @@ class EmojiMemoryGame{
 
     //注意这里在使用 Card 时，使用的是 外部类.内部类 的形式获取的 Card，因为 Card 是嵌套类型
     //这里的 choose 就是 MVVM 中 VM 的意图 intent
-    func choose(card: MemoryGame<String>.Card){
+    func choose(card: MemoryGame<String>.Card) {
         //这里可以不写形参的原因是 model 中的chooseCard将参数外部名称设置为了 _
         model.chooseCard(card)
 
--- swiftui/Memorize03/Memorize03/MemorizeGame.swift
+++ swiftui/Memorize03/Memorize03/MemorizeGame.swift
@@ -4,26 +4,36 @@
 //
 //  Created by shanquan on 2024/12/7.
 //
-// 让整个记忆游戏都是泛型的
-struct MemoryGame<CardContent>{
-    //private 让 cards 只能在这个结构体中访问，否则其他类可以直接修改 cards
-    //所有的行为都应该通过这个结构体的方法来实现
+// 让 当前 Model 都是泛型的
+struct MemoryGame<CardContent> {
+    // private 让 cards 只能在这个结构体中访问，否则其他类可以直接修改 cards
+    // 所有的行为都应该通过这个结构体的方法来实现
     // private(set) 会将 set 方法设置为私有, get 依然是公有的
-    private(set) var cards: Array<Card>
+    private(set) var cards: [Card]
 
-    //如果要初始化
-    init(numberofPairsOfCards:Int){
-        cards = Array<Card>()
+    // cardContentFactory 函数式编程的例子，通过传递cardContentFactory函数，model 就不用关心 content 的具体类型了
+    // 其实也可以传泛型
+    init(
+        numberofPairsOfCards: Int,
+        cardContentFactory: (Int) -> CardContent
+    ) {
+        cards = [Card]()
+        for pairIndex in 0..<numberofPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
     }
 
-    func chooseCard(_ card:Card){
+    func chooseCard(_ card: Card) {
 
     }
 
     //声明一个泛型类型的 Card，会使用 CardContent 作为内容
-    struct Card{
-        var isFaceUp:Bool = true
-        var isMatched:Bool = false
-        var content: CardContent
+    struct Card {
+        var isFaceUp = false
+        var isMatched = false
+        //内容不会随着游戏进行而变化，所以是 let
+        let content: CardContent
     }
 }

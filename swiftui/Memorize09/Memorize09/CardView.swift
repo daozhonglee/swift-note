c2c06c12c6d8667380ef98d4e7a49de35d0e2ebd
tianfav <tianfav@163.com>
2024-12-12 00:09:47 +0800

    lecture09 制作了数字飞出的效果,并且解决了卡片匹配时同时翻转和小时的问题和一排卡片都匹配时下面一行向上顶的问题

--- swiftui/Memorize09/Memorize09/CardView.swift
+++ swiftui/Memorize09/Memorize09/CardView.swift
@@ -16,24 +16,37 @@ struct CardView: View {
     }
 
     var body: some View {
-        Pie(endAngle: .degrees(240))
-            .opacity(Constants.Pie.opacity)
-            .overlay(
-                Text(card.content)
-                    .font(.system(size: Constants.FontSize.largest))
-                    .minimumScaleFactor(Constants.FontSize.scaleFactory)
-                    .multilineTextAlignment(.center)
-                    .aspectRatio(1, contentMode: .fit)
-                    .padding(Constants.Pie.inset)
-                    .rotationEffect(.degrees(card.isMatched ? 360 : 0))
-                    .animation(
-                        .spin(duration: 1),
-                        value: card.isMatched
-                    )
+        //动画将调用它的频率与它正在制作的动画通道同步。
+        TimelineView(.animation) { timeline in
+            //下面这个是为了解决当点击匹配的卡片时，同时反转和褪色的情况
+            if card.isFaceUp || !card.isMatched {
+                Pie(endAngle: .degrees(card.bonusPercentRemaining * 360))
+                    .opacity(Constants.Pie.opacity)
+                    .overlay(cardContents.padding(Constants.Pie.inset))
+                    .padding(Constants.inset)
+                    .cardify(isFaceUp: card.isFaceUp)
+                //不透明度是在 ViewHierarchy 中删除或添加视图时使用的默认过渡。
+                    .transition(.opacity) //默认是这样，不写也会有这个效果，可以改成其他的试试
+//                    .transition(.scale)
+                //这里的不透明度是为了在没有内容的时候，仍然占据空间
+//                    .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+            }else{
+                //
+                Color.clear
+            }
+        }
+    }
+    var cardContents: some View {
+        Text(card.content)
+            .font(.system(size: Constants.FontSize.largest))
+            .minimumScaleFactor(Constants.FontSize.scaleFactory)
+            .multilineTextAlignment(.center)
+            .aspectRatio(1, contentMode: .fit)
+            .rotationEffect(.degrees(card.isMatched ? 360 : 0))
+            .animation(
+                .spin(duration: 1),
+                value: card.isMatched
             )
-            .padding(Constants.inset)
-            .cardify(isFaceUp: card.isFaceUp)
-            .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 
     private struct Constants {
@@ -61,9 +74,9 @@ struct CardView: View {
     .foregroundColor(.orange)
 }
 
-extension Animation{
+extension Animation {
     //利用现有动画创建一个自己的动画
-    static func spin(duration: TimeInterval)-> Animation{
+    static func spin(duration: TimeInterval) -> Animation {
         .linear(duration: duration).repeatForever(autoreverses: false)
     }
 }

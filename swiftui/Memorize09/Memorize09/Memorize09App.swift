4b88c10ef2a7a19768f1faf8a83ca2f9fb58931e
tianfav <tianfav@163.com>
2024-12-11 22:30:04 +0800

    lecture09 init

---
+++ swiftui/Memorize09/Memorize09/Memorize09App.swift
@@ -0,0 +1,18 @@
+//
+//  Memorize09App.swift
+//  Memorize09
+//
+//  Created by shanquan on 2024/12/11.
+//
+
+import SwiftUI
+
+@main
+struct Memorize09App: App {
+    @StateObject var game = EmojiMemoryGame()
+    var body: some Scene {
+        WindowGroup {
+            EmojiMomoryGameView(viewModel: game)
+        }
+    }
+}

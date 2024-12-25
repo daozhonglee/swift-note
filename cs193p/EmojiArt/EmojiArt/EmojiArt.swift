c70f8e36e4080988eae3bfb8b9e8aa75f9d7a477
tianfav <tianfav@163.com>
2024-12-25 21:54:33 +0800

    rename

---
+++ cs193p/EmojiArt/EmojiArt/EmojiArt.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  EmojiArt
+//
+//  Created by shanquan on 2024/12/14.
+//
+
--- cs193p/EmojiArt/EmojiArt/EmojiArtApp.swift
+++ cs193p/EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -1,14 +1,14 @@
 //
-//  emojiArtApp.swift
-//  emojiArt
+//  EmojiArtApp.swift
+//  EmojiArt
 //
-//  Created by shanquan on 2024/11/23.
+//  Created by shanquan on 2024/12/14.
 //
 
 import SwiftUI
 
 @main
-struct emojiArtApp: App {
+struct EmojiArtApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()

891c2a35c6c44959985f6d3b761419fd0a7cada7
tianfav <tianfav@163.com>
2024-12-29 19:14:35 +0800

    update

---
+++ swiftui/VideoRecod/VideoRecod/VideoRecodApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoRecodApp.swift
+//  VideoRecod
+//
+//  Created by shanquan on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct VideoRecodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
